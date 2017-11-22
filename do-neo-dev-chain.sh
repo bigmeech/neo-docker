ea2b6a6318b7d7409f79cd29e62e3189d25052c9
henryfour <me@henryfour.com>
2017-11-22 14:34:09 +0800

    add test command for neo-dev-chain

--- do-neo-dev-chain.sh
+++ do-neo-dev-chain.sh
@@ -2,6 +2,7 @@
 
 vDockerImage="neo-dev-chain:latest"
 vContainerName="neo-dev-chain"
+vRpcPort=4201
 
 function run() {
     if [[ -n $(docker ps -aq -f name=$vContainerName) ]]; then
@@ -9,7 +10,8 @@ function run() {
     else
         docker run --name $vContainerName \
             --cpus 1 \
-            -dit -p 127.0.0.1:4001-4004:4001-4004 -p 4101:4101 -p 4201:4201 \
+            -h "neo-dev-chain" \
+            -dit -p 127.0.0.1:4001-4004:4001-4004 -p 4101:4101 -p $vRpcPort:$vRpcPort \
             --restart no \
             $vDockerImage
     fi
@@ -17,6 +19,11 @@ function run() {
 
 function usage() {
     echo "Usage: $0 [run | start | attach | bash | stop | rm]"
+}
+
+function rpc() {
+    curl -d "$1" http://localhost:$vRpcPort
+    echo
 }
 
 if [[ $# == 0 ]]; then
@@ -40,6 +47,9 @@ else
         "rm")
             docker stop $vContainerName
             docker rm $vContainerName
+            ;;
+        "test")
+            rpc '{"jsonrpc": "2.0", "method": "getblockcount", "params": [], "id": 5}'
             ;;
         *) 
             usage
